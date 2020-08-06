a5ad155fac715b80fc47ca77fc06106776966fa8
withHappy <hap2.dev@gmail.com>
2020-08-06 15:38:50 +0900

    feat : automated testing

--- celery_test/celery_test/settings.py
+++ celery_test/celery_test/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+        }
+
     }
 }
 
